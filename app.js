7f0c9d6442d8475550ea3c8af96e127e4605becb
Nick Jacoby <nickjacoby22@gmail.com>
2016-06-07 18:45:24 -0700

    created random customer method per hour and created first object literal pikePlaceMarket

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 //Beginning of Campfire-Coffee
+var pikePlaceMarket = {
+  maxCustPerHour: 35,
+  minCustPerHour: 14,
+
+  getCustPerHour: function() {
+    custPerHour = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1) + this.minCustPerHour);
+    return custPerHour;
+  },
+};
+
+pikePlaceMarket.getCustPerHour();
