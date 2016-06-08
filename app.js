1392bbcaf8f18b6b16ca92b353d0ae748d5e85b0
Nick Jacoby <nickjacoby22@gmail.com>
2016-06-07 19:07:37 -0700

    Populated array with random customers each hour

--- app.js
+++ app.js
@@ -2,11 +2,17 @@
 var pikePlaceMarket = {
   maxCustPerHour: 35,
   minCustPerHour: 14,
+  hoursOfOperation: 15,
+  
+  custEachHour: [],
 
-  getCustPerHour: function() {
-    custPerHour = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1) + this.minCustPerHour);
-    return custPerHour;
+  fillCustEachHour: function() {
+    for (var i = 0; i < this.hoursOfOperation; i++) {
+      custPerHour = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1) + this.minCustPerHour);
+      this.custEachHour.push(custPerHour);
+    }
   },
 };
 
-pikePlaceMarket.getCustPerHour();
+pikePlaceMarket.fillCustEachHour();
+console.log(pikePlaceMarket.custEachHour);
