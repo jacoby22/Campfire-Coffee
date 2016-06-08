63b9aa913eb7959dc4b2e2cfc3fa133a367bb8dd
Nick Jacoby <nickjacoby22@gmail.com>
2016-06-07 20:02:12 -0700

    added elements in loop to populate cupsEachHour and lbsEachHour and created variables for storing running totals of each

--- app.js
+++ app.js
@@ -3,16 +3,41 @@ var pikePlaceMarket = {
   maxCustPerHour: 35,
   minCustPerHour: 14,
   hoursOfOperation: 15,
-  
+  cupsPerCust: 1.2,
+  lbsPerCust: 0.34,
+  sumOfCupsPerDay: 0,
+  sumOfLbsPerDay: 0,
+  coffeeYield: 16,
   custEachHour: [],
+  cupsEachHour: [],
+  lbsEachHour: [],
+  beansEachHour: [],
 
-  fillCustEachHour: function() {
+
+  fillEachHourArr: function() {
+    var sumOfCups = 0.0;
+    var sumOfLbs = 0.0;
     for (var i = 0; i < this.hoursOfOperation; i++) {
       custPerHour = Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1) + this.minCustPerHour);
       this.custEachHour.push(custPerHour);
+      this.cupsEachHour.push(Math.round((custPerHour * this.cupsPerCust) * 10) / 10);
+      this.lbsEachHour.push(Math.round((custPerHour * this.lbsPerCust) * 10) / 10);
+      sumOfCups += Math.round((custPerHour * this.cupsPerCust) * 10) / 10;
+      sumOfLbs += Math.round((custPerHour * this.cupsPerCust) * 10) / 10;
+
     }
+    this.sumOfCupsPerDay = Math.ceil(sumOfCups);
+    this.sumOfLbsPerDay = Math.ceil(sumOfLbs);
   },
+
+  fillBeansEachHourArr: function() {
+    var sumOfTotalLbs = 0;
+    for (value in lbsEachHour) {
+    }
+  }
 };
 
-pikePlaceMarket.fillCustEachHour();
+pikePlaceMarket.fillEachHourArr();
 console.log(pikePlaceMarket.custEachHour);
+console.log(pikePlaceMarket.cupsEachHour);
+console.log(pikePlaceMarket.lbsEachHour);
